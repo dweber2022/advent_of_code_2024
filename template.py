c30649c01916b76d7c2d7afe9c6c1889eaab514f
dcheng <102439573+dcheng2022@users.noreply.github.com>
2024-12-02 23:35:45 -0600

    update template

--- template.py
+++ template.py
@@ -1,11 +1,21 @@
+import unittest 
+
 def read_file(filename):
     with open(filename, mode="r") as f:
         content = f.read().strip()
         return content
 
-def main():
-    content = read_file()
+def main(filename):
+    content = read_file(filename)
+
+class Test(unittest.TestCase):
+    filename = ""
+
+    def test_1_small(self):
+        out = None
+        self.assertEqual(main(filename), out)
 
 if __name__ == "__main__":
+    unittest.main()
     result = main()
     print(result)
