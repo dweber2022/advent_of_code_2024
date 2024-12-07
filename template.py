9990e6b7927d2194fd5b465a67f42bd17ae035fb
Darin Weber <darinweber@u.northwestern.edu>
2024-12-07 13:06:00 -0600

    update template

--- template.py
+++ template.py
@@ -1,3 +1,4 @@
+from aoclib import *
 import itertools as it
 import functools as ft
 import unittest 
@@ -7,15 +8,32 @@ def read_file(filename):
         content = f.read().strip()
         return content
 
-def main(filename):
+def part1(filename):
     content = read_file(filename)
+
+def part2(filename):
+    content = read_file(filename)
+
+def main(filename):
+    return (
+        part1(filename),
+        part2(filename)
+    )
 
 class Test(unittest.TestCase):
     filename = ""
 
+    @staticmethod
+    def set_filename(filename):
+        Test.filename = filename
+
     def test_1_small(self):
         out = None
-        self.assertEqual(main(filename), out)
+        self.assertEqual(part1(Test.filename), out)
+
+    def test_2_small(self):
+        out = None
+        self.assertEqual(part2(Test.filename), out)
 
 def pprint(message):
     print("="*50, end="\n\n")
@@ -23,6 +41,10 @@ def pprint(message):
     print("="*50)
  
 if __name__ == "__main__":
+    day = ""
+    small_filename = f"{day}.small.in"
+    filename = f"{day}.in"
+    Test.set_filename(small_filename)
     unittest.main(exit=False)
-    result = main("")
-    pprint(result)
+    results = main(filename)
+    pprint(results)
