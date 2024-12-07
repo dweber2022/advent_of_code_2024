90ff0a953e6f0b0bd289afb12d9c45eba5a64b07
Darin Weber <darinweber@u.northwestern.edu>
2024-12-07 13:06:53 -0600

    add matrix parsing

--- aoclib/src/aoclib/parse.py
+++ aoclib/src/aoclib/parse.py
@@ -4,3 +4,8 @@ def parse_list(content, func=None, delim=" "):
     else:
         content_as_list = list(map(func, content.split(delim)))
     return content_as_list
+
+def parse_matrix(content, func=None, col_delim=" ", row_delim="\n"):
+    rows = content.split(row_delim)
+    matrix = [parse_list(row, func, col_delim) for row in rows]
+    return matrix
